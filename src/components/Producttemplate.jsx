f822f2316d8a8ac47f21d5ee90160f4234fd492e
Diptanshu Dey <diptanshudey4@gmail.com>
2024-11-22 17:47:20 +0530

    product page

--- src/components/Producttemplate.jsx
+++ src/components/Producttemplate.jsx
@@ -5,82 +5,170 @@ import tempThree from '../imgs/Frame 76.png'
 import tempFour from '../imgs/Frame 72.png'
 function Producttemplate() {
     return (
-        <div className='tempmain container d-flex justify-content-around'>
-            <div className='tempinner d-flex justify-content-around align-items-center'>
-                <div className='innerone d-flex flex-column'>
-                    <img src={tempOne} alt="..." />
-                    <img src={tempTwo} alt="..." />
-                    <img src={tempThree} alt="..." />
+        <>
+            <div className='tempmain container d-flex justify-content-around'>
+                <div className='tempinner d-flex justify-content-around align-items-center'>
+                    <div className='innerone d-flex flex-column'>
+                        <img src={tempOne} alt="..." />
+                        <img src={tempTwo} alt="..." />
+                        <img src={tempThree} alt="..." />
+                    </div>
+                    <div className='innertwo'>
+                        <img src={tempFour} alt="..." />
+                    </div>
                 </div>
-                <div className='innertwo'>
-                    <img src={tempFour} alt="..." />
-                </div>
-            </div>
-            <div className='innerthree d-flex align-self-center flex-column'>
-                <h1> PINK TEXTURED SAREE</h1>
-                <p>
-                    INR 1399
-                </p>
-                <p>
-                    Inclusive of all taxes
-                </p>
-                {/* offer section starts from here */}
-                <div className='offers w-75'>
-                    <i className='bx bxs-offer mx-2'></i>Get<p>
-                        this for INR 989
-                        Flat 10% Off your first purchase. Download the app and use
-                        Code: APP10
+                <div className='innerthree d-flex align-self-center flex-column'>
+                    <h1> PINK TEXTURED SAREE</h1>
+                    <p>
+                        INR 1399
                     </p>
-                    <i className='bx bxs-offer mx-2'></i>Get<p>
-                        this for INR 989
-                        Flat 10% Off your first purchase. Download the app and use
-                        Code: APP10
+                    <p>
+                        Inclusive of all taxes
                     </p>
-                    <i className='bx bxs-offer mx-2'></i>Get<p>
-                        this for INR 989
-                        Flat 10% Off your first purchase. Download the app and use
-                        Code: APP10
-                    </p>
-                </div>
-                {/* offer section ends her */}
+                    {/* offer section starts from here */}
+                    <div className='offers w-75'>
+                        <div className='offer-one d-flex align-items-center'>
+                            <i className='bx bxs-offer mx-2'></i><p>
+                                Get this for INR 98
+                                Flat 10% Off your first purchase. Download the app and use
+                                Code: APP10
+                            </p>
+                        </div>
+                        <div className='offer-one d-flex align-items-center'>
+                            <i className='bx bxs-offer mx-2'></i><p>
+                                Get this for INR 989
+                                Flat 10% Off your first purchase. Download the app and use
+                                Code: APP10
+                            </p>
+                        </div>
+                        <div className='offer-one d-flex align-items-center'>
+                            <i className='bx bxs-offer mx-2'></i><p>
+                                Get this for INR 989
+                                Flat 10% Off your first purchase. Download the app and use
+                                Code: APP10
+                            </p>
+                        </div>
+                    </div>
+                    {/* offer section ends her */}
 
-                {/* product headers starts from here */}
-                <div className='main-product-headers d-flex justify-content-between'>
-                    <div className='size-div'>
-                        <p>Select a size</p>
-                        <div className='sizes d-flex justify-content-between'>
-                            <p>S</p>
-                            <p>M</p>
-                            <p>L</p>
-                            <p>XL</p>
+                    {/* product headers starts from here */}
+                    <div className='main-product-headers d-flex justify-content-between'>
+                        <div className='size-div'>
+                            <p>Select a size</p>
+                            <div className='sizes d-flex justify-content-between'>
+                                <p>S</p>
+                                <p>M</p>
+                                <p>L</p>
+                                <p>XL</p>
+                            </div>
+                        </div>
+                        <div className='quanitity-div' >
+                            <p>Quanitity</p>
+                            <div className='quantity-div-inner d-flex justify-content-between align-items-center'>
+                                <p>-</p>
+                                <p>0</p>
+                                <p>+</p>
+                            </div>
+                        </div>
+                        <div className='total-div d-flex flex-column'>
+                            <p>Total</p>
+                            <div>
+                                INR 1,299
+                            </div>
                         </div>
                     </div>
-                    <div className='quanitity-div' >
-                        <p>Quanitity</p>
-                        <div className='quantity-div-inner d-flex justify-content-between align-items-center'>
-                            <p>-</p>
-                            <p>0</p>
-                            <p>+</p>
-                        </div>
+                    {/* product headers end here */}
+                    <div className='actionb mt-1'>
+                        <button id='addtocart' className='mb-1'>
+                            <a href="/">Add to cart</a>
+                        </button>
+                        <button id='buynow' className='mb-1'>
+                            <a href="/">Buy Now</a>
+                        </button>
                     </div>
-                    <div className='total-div d-flex flex-column'>
-                        <p>Total</p>
-                        <div>
-                            INR 1,299
-                        </div>
-                    </div>
-                </div>
-                {/* product headers end here */}
-                <div className='actionb mt-1'>
-                    <button id='addtocart' className='mb-1'>
-                        <a href="/">Add to cart</a>
-                    </button>
-                    <button id='buynow' className='mb-1'>
-                        <a href="/">Buy Now</a>
-                    </button>
                 </div>
             </div>
-        </div >
+            <div className='reviews-main container w-100 d-flex justify-content-between'>
+                <div className='reviews-left w-50'></div>
+                <div className='reviews-right w-50'>
+                    <div>
+                        <h3>Product Description</h3>
+                        <p>
+                            This Saree Is Suitable To Get A Contemporary Stylish Look In Normal
+                            Occasions, College Farewell, Family Get Together, Regular Or Daily Use,
+                            Office Or Work Or Gifting To Loved One.
+                        </p>
+                        <p>
+                            This Saree Is Suitable To Get A Contemporary Stylish Look In Normal
+                            Occasions, College Farewell, Family Get Together, Regular Or Daily Use,
+                            Office Or Work Or Gifting To Loved One.
+                        </p>
+                    </div>
+                    <div className='pro-details'>
+                        <div>
+                            <h3>Product Details</h3>
+                        </div>
+                        <div className='pro-contents d-flex justify-content-between'>
+                            <div>
+                                <p>Material Composition</p>
+                                <p>Weave Type</p>
+                                <p>Included Components</p>
+                                <p>Product Manufacturer</p>
+                            </div>
+                            <div>
+                                <p>Silk</p>
+                                <p>Jacquard Type</p>
+                                <p>With blouse piece</p>
+                                <p>Leemboodi Fashion</p>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+            {/* half page completion */}
+            <div className='secondmain px-4'>
+               <h1>You may also like</h1> 
+               <div className='secondmain-inner d-flex justify-content-between'>
+                <div className='innerCard'>
+                    <img src='' href='/'></img>
+                    <p>
+                    Cotton Saree at minimal cost than you can even imagine.
+                    </p>
+                    <p>
+                    INR 2,999
+                    </p>
+                </div>
+                <div className='innerCard'>
+                    <img src='' href='/'></img>
+                    <p>
+                    Cotton Saree at minimal cost than you can even imagine.
+                    </p>
+                    <p>
+                    INR 2,999
+                    </p>
+                </div>
+                <div className='innerCard'>
+                    <img src='' href='/'></img>
+                    <p>
+                    Cotton Saree at minimal cost than you can even imagine.
+                    </p>
+                    <p>
+                    INR 2,999
+                    </p>
+                </div>
+                <div className='innerCard'>
+                    <img src='' href='/'></img>
+                    <p>
+                    Cotton Saree at minimal cost than you can even imagine.
+                    </p>
+                    <p>
+                    INR 2,999
+                    </p>
+                </div>
+               </div>
+            </div>
+        </>
     )
 }
 export default Producttemplate
