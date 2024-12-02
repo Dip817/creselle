dcc0a23e3729799d795e766bb54829495b18b4db
Diptanshu Dey <diptanshudey4@gmail.com>
2024-12-02 17:07:05 +0530

    prod page

--- src/components/Productpage.jsx
+++ src/components/Productpage.jsx
@@ -71,19 +71,115 @@ function Productpage() {
                 </div>
                 {/* half page completion */}
             </div>
-            <div className='lower-main'>
-                <div className='lower-main-left justify-content-end d-flex' >
-                    <div className='lower-main-left-headers d-flex flex-column  mt-5' >
+            <div className='lower-main d-flex' style={{ gap: '15px' }}>
+                {/* left div beginning */}
+                <div className='lower-main-left justify-content-end d-flex flex-column' style={{ paddingLeft: '15%' }} >
+
+                    <div className='lower-main-left-headers d-flex flex-column  mt-5' style={{ width: '100%' }}>
                         <div className='d-flex'>
-                            <p style={{border:'1px solid black',margin:'0px'}}>Returns</p>
-                            <p style={{margin:'0px'}}>Our Promise</p>
+                            <p style={{ border: '1px solid black', margin: '0px' }}>Returns</p>
+                            <p style={{ margin: '0px', paddingLeft: '1%', paddingTop: '2px' }}>Our Promise</p>
                         </div>
                         <div>
-                            <p style={{border:'1px solid black' }}>7 day Return and Exchange <a href="/" style={{ textDecoration: 'none' }}>click here.</a></p>
+                            <p style={{ border: '1px solid black', padding: '5%' }}>7 day Return and Exchange <a href="/" style={{ textDecoration: 'none' }}>click here.</a></p>
+                        </div>
+                    </div>
+                    <div className='lower-main-left-bottom'>
+                        <div className='lower-main-left-bottom-up mt-5'>
+                            <div><p>Reviews and Ratings</p></div>
+                            <div>
+                                <i class='bx bxs-star'></i>
+                                <i class='bx bxs-star'></i>
+                                <i class='bx bxs-star'></i>
+                                <i class='bx bxs-star'></i>
+                                <i class='bx bxs-star'></i>
+                                <button id='reviews'>
+                                    <a href="/">(39 reviews)</a>
+                                </button>  </div>
+                        </div>
+                        <div className='lower-main-left-bottom-down d-flex justify-content-between'>
+                            <div className='lower-main-left-bottom-down-left'>
+                                <h1>4.1</h1>
+                                <p>420 Customers</p>
+                            </div>
+                            <div className='d-flex align-items-center flex-column'> <div className='lower-main-left-bottom-down-right'>
+                                <i class='bx bxs-star'></i>
+                                <i class='bx bxs-star'></i>
+                                <i class='bx bxs-star'></i>
+                                <i class='bx bxs-star'></i>
+                                <i class='bx bxs-star'></i>
+                            </div>
+                                <button id='Write-R'>WRITE A REVIEW</button></div>
                         </div>
                     </div>
                 </div>
-                <div className='lower-main-right'></div>
+                {/* right div beginning */}
+                <div className='lower-main-right'>
+                    <h5 style={{ marginTop: '10%' }}>Product Description</h5>
+                    <p>
+                        This Saree Is Suitable To Get A Contemporary Stylish Look In Normal
+                        Occasions, College Farewell, Family Get Together, Regular Or Daily Use,
+                        Office Or Work Or Gifting To Loved One.</p>
+                    <p> Colour Of The Actual Product May Slightly Vary Due To Different Photographic Lighting Sources Or Your Display Color Settings Or Screen Type
+                    </p>
+                    <h5>Product Details</h5>
+                    <div className='d-flex justify-content-between'>
+                        <div>
+                            <p>Material Composition</p>
+                            <p>Weave Type</p>
+                            <p>Included Components</p>
+                            <p>Product Manufacturer</p>
+                        </div>
+                        <div>
+                            <p>Silk</p>
+                            <p>Jacquard Type</p>
+                            <p>With blouse piece</p>
+                            <p>Leemboodi Fashion</p>
+                        </div>
+                    </div>
+                </div>
+            </div >
+            {/* above footer section beginning */}
+            <div className='' style={{marginTop:'5%'}}>
+                <h1>You may also like</h1>
+                <div className='second-lower d-flex justify-content-between' style={{marginTop:'5%'}}>
+                    <div className='second-lower-inner'>
+                        <img src='' href alt='/'></img>
+                        <p>
+                            Cotton Saree at minimal cost than you can even imagine.
+                        </p>
+                        <p>
+                            INR 2,999
+                        </p>
+                    </div>
+                    <div className='second-lower-inner'>
+                        <img src='' href alt='/'></img>
+                        <p>
+                            Cotton Saree at minimal cost than you can even imagine.
+                        </p>
+                        <p>
+                            INR 2,999
+                        </p>
+                    </div>
+                    <div className='second-lower-inner'>
+                        <img src='' href alt='/'></img>
+                        <p>
+                            Cotton Saree at minimal cost than you can even imagine.
+                        </p>
+                        <p>
+                            INR 2,999
+                        </p>
+                    </div>
+                    <div className='second-lower-inner'>
+                        <img src='' href alt='/'></img>
+                        <p>
+                            Cotton Saree at minimal cost than you can even imagine.
+                        </p>
+                        <p>
+                            INR 2,999
+                        </p>
+                    </div>
+                </div>
             </div>
         </>
 
