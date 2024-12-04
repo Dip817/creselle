0636becedacc7e582a77d0c7944cad7a27e41548
Diptanshu Dey <diptanshudey4@gmail.com>
2024-12-04 18:25:27 +0530

    admin page

--- src/components/Admin.jsx
+++ src/components/Admin.jsx
@@ -11,12 +11,12 @@ function Admin() {
             {/* main body beginning */}
             <div className='mainbody w-100 d-flex'>
                 {/* leftdiv beginning */}
-                <div className='mainbody-left d-flex flex-column' style={{ width: '15%', backgroundColor: 'yellowgreen', gap: '50px' }}>
-                    <div className='dash d-flex align-items-center' style={{ paddingLeft: '25%', gap: '8px' }}>
+                <div className='mainbody-left d-flex flex-column py-5' style={{ width: '15%', backgroundColor: 'yellowgreen', gap: '50px' }}>
+                    <div className='dash d-flex align-items-center active' style={{ paddingLeft: '25%', gap: '8px' }}>
                         <i class='bx bxs-home'></i>
                         <p>Dashboard</p>
                     </div>
-                    <div className='use d-flex align-items-center' style={{ paddingLeft: '25%', gap: '8px' }}>
+                    <div className='use d-flex align-items-center ' style={{ paddingLeft: '25%', gap: '8px' }}>
                         <i class='bx bxs-user'></i>
                         <p>Users</p>
                     </div>
@@ -34,9 +34,9 @@ function Admin() {
                     </div>
                 </div>
                 {/* rightdiv beginning */}
-                <div className='mainbody-right' style={{ width: '85%', backgroundColor: 'black' }}>
+                <div className='mainbody-right p-5' style={{ width: '85%'}}>
                     <h4 style={{ color: 'white', paddingLeft: '5%', paddingTop: '3%' }}>All Products on Site</h4>
-                    <div className='mainbody-right-box' style={{ backgroundColor: 'white', borderRadius: '10px' }}>
+                    <div className='mainbody-right-box p-5 d-flex flex-column' style={{ backgroundColor: 'white', borderRadius: '10px', gap:'20px' }}>
                         <div className='mainbody-right-boxheaders d-flex justify-content-around' style={{ color: '#718EBF', marginBottom: '1px solid #E6EFF5' }}>
                             <h6>Description</h6>
                             <h6>Transaction ID</h6>
@@ -46,30 +46,44 @@ function Admin() {
                             <h6>Receipt</h6>
                         </div>
                         <div className='products d-flex justify-content-around' style={{ marginBottom: '1px solid #E6EFF5' }}>
-                        <p>Saree</p>
-                        <p>#12548796</p> 
-                        <p>Shopping</p>
-                        <p>28 Jan</p>
-                        <p>$2,500</p>
-                        <button id='receipt'>
-                                <a href="/">Edit</a>
-                            </button>
-                        </div>                       
+                            <p>Saree</p>
+                            <p>#12548796</p>
+                            <p>Shopping</p>
+                            <p>28 Jan</p>
+                            <p>$2,500</p>
+                            <button id='receipt'>Edit</button>
+                        </div>
                     </div>
                     <div className='mainbody-right-lower-div d-flex justify-content-around mt-5'>
-                            <div className='bal'style={{background:'white',width:'20%',borderRadius:'20px'}}>
-                            <i class='bx bxs-wallet'></i><p>My Balance</p>
-                            <p>$12,750</p>
+                        <div className='bal d-flex align-items-center justify-content-around' style={{ background: 'white', width: '20%', borderRadius: '20px' }}>
+                            <div className="admin-dashboard-icon">
+                                <i class='bx bxs-wallet'></i>
                             </div>
-                            <div className='inc'style={{background:'white',width:'20%',borderRadius:'20px'}}>
-                            <i class='bx bx-dollar-circle'></i>
-                            <p>Income</p>
-                            </div>
-                            <div className='exp'style={{background:'white',width:'20%',borderRadius:'20px'}}>
-                            <i class='bx bxs-receipt'></i>
-                            <p>Expense</p>
+                            <div className="d-flex flex-column justify-content-center">
+                                <p>My Balance</p>
+                                <p>$12,750</p>
                             </div>
                         </div>
+                        <div className='bal d-flex align-items-center justify-content-around' style={{ background: 'white', width: '20%', borderRadius: '20px' }}>
+                            <div className="admin-dashboard-icon">
+                                <i class='bx bx-money'></i>
+                            </div>
+                            <div className="d-flex flex-column justify-content-center">
+                                <p>My Income</p>
+                                <p>$12,750</p>
+                            </div>
+                        </div>
+                        <div className='bal d-flex align-items-center justify-content-around' style={{ background: 'white', width: '20%', borderRadius: '20px' }}>
+                            <div className="admin-dashboard-icon">
+                            <i class='bx bxs-receipt'></i>
+                            </div>
+                            <div className="d-flex flex-column justify-content-center">
+                                <p>My Expense</p>
+                                <p>$12,750</p>
+                            </div>
+                        </div>
+                        
+                    </div>
                 </div>
             </div>
         </>
