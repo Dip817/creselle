4472ef5277f4d8ddd90d3cb46e3c730ade991f70
Diptanshu Dey <diptanshudey4@gmail.com>
2024-12-04 17:58:55 +0530

    admin page

--- src/components/Admin.jsx
+++ src/components/Admin.jsx
@@ -36,8 +36,8 @@ function Admin() {
                 {/* rightdiv beginning */}
                 <div className='mainbody-right' style={{ width: '85%', backgroundColor: 'black' }}>
                     <h4 style={{ color: 'white', paddingLeft: '5%', paddingTop: '3%' }}>All Products on Site</h4>
-                    <div className='mainbody-right-box' style={{ backgroundColor: 'white', borderRadius: '10px'}}>
-                        <div className='mainbody-right-boxheaders d-flex justify-content-around' style={{ color: '#718EBF',marginBottom:'1px solid #E6EFF5'}}>
+                    <div className='mainbody-right-box' style={{ backgroundColor: 'white', borderRadius: '10px' }}>
+                        <div className='mainbody-right-boxheaders d-flex justify-content-around' style={{ color: '#718EBF', marginBottom: '1px solid #E6EFF5' }}>
                             <h6>Description</h6>
                             <h6>Transaction ID</h6>
                             <h6>Type</h6>
@@ -45,8 +45,31 @@ function Admin() {
                             <h6>Amount</h6>
                             <h6>Receipt</h6>
                         </div>
-
+                        <div className='products d-flex justify-content-around' style={{ marginBottom: '1px solid #E6EFF5' }}>
+                        <p>Saree</p>
+                        <p>#12548796</p> 
+                        <p>Shopping</p>
+                        <p>28 Jan</p>
+                        <p>$2,500</p>
+                        <button id='receipt'>
+                                <a href="/">Edit</a>
+                            </button>
+                        </div>                       
                     </div>
+                    <div className='mainbody-right-lower-div d-flex justify-content-around mt-5'>
+                            <div className='bal'style={{background:'white',width:'20%',borderRadius:'20px'}}>
+                            <i class='bx bxs-wallet'></i><p>My Balance</p>
+                            <p>$12,750</p>
+                            </div>
+                            <div className='inc'style={{background:'white',width:'20%',borderRadius:'20px'}}>
+                            <i class='bx bx-dollar-circle'></i>
+                            <p>Income</p>
+                            </div>
+                            <div className='exp'style={{background:'white',width:'20%',borderRadius:'20px'}}>
+                            <i class='bx bxs-receipt'></i>
+                            <p>Expense</p>
+                            </div>
+                        </div>
                 </div>
             </div>
         </>
